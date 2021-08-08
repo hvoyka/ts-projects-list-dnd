576b5405158398dfb8e262701558a421e05f382e
Lavrentev Stanislav <hvoinii@gmail.com>
2021-08-08 14:57:16 +0600

    feat: filter projects and fix duplicates projects

--- src/app.ts
+++ src/app.ts
@@ -128,7 +128,14 @@ class ProjectList {
     this.element.id = `${this.type}-projects`;
 
     projectState.addListener((projects: Project[]) => {
-      this.assignedProjects = projects;
+      const relevantProjects = projects.filter((prj) => {
+        if (this.type === "active") {
+          return prj.status === ProjectStatus.Active;
+        }
+        return prj.status === ProjectStatus.Finished;
+      });
+
+      this.assignedProjects = relevantProjects;
       this.renderProjects();
     });
 
@@ -140,6 +147,8 @@ class ProjectList {
     const listEl = <HTMLUListElement>(
       document.getElementById(`${this.type}-projects-list`)!
     );
+
+    listEl.innerHTML = "";
 
     for (const prjItem of this.assignedProjects) {
       const listItem = document.createElement("li");
