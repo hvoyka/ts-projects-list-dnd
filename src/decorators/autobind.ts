9b83b89f5364a6899da36add3ca1c801457f3242
Lavrentev Stanislav <hvoinii@gmail.com>
2021-08-09 20:51:06 +0600

    fix: use namespaces for split code to files

---
+++ src/decorators/autobind.ts
@@ -0,0 +1,17 @@
+namespace App {
+  export function autobind(
+    _target: any,
+    _methodName: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFc = originalMethod.bind(this);
+        return boundFc;
+      },
+    };
+    return adjDescriptor;
+  }
+}
